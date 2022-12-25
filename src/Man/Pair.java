7c5b88165a20508a48246114a798617b9e7bb488
hazemAmr0 <109316536+hazemAmr0@users.noreply.github.com>
2022-12-25 02:20:50 +0200

    update for pair class

--- src/Man/Pair.java
+++ src/Man/Pair.java
@@ -15,5 +15,29 @@ package Man;
     public int getY() {
         return y;
     }
+     @Override
+     public int hashCode() {
+         int hash = 3;
+         hash = 47 * hash + this.x;
+         hash = 47 * hash + this.y;
+         return hash;
+     }
 
+     @Override
+     public boolean equals(Object obj) {
+         if (obj == null) {
+             return false;
+         }
+         if (getClass() != obj.getClass()) {
+             return false;
+         }
+         final Pair other = (Pair) obj;
+         if (this.x != other.x) {
+             return false;
+         }
+         if (this.y != other.y) {
+             return false;
+         }
+         return true;
+     }
 }
